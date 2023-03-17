1afd6f741fb4f3450dcc3ef18284defff16493c7
ferryboat <ferryboatseranade@gmail.com>
2023-03-17 22:19:20 +0800

    进度条功能：和http请求结合到一起了。

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,6 +1,7 @@
 //对于axios进行二次封装
 import axios from "axios";
-
+import nprogress from "nprogress";
+import "nprogress/nprogress.css";
 //axios.create方法执行,其实返回一个axios和request一样的
 let requests = axios.create({
     //基础路径,发请求URL携带api【发现:真实服务器接口都携带/api】
@@ -11,12 +12,14 @@ let requests = axios.create({
 //请求拦截器:将来项目中【N个请求】，只要发请求,会触发请求拦截器!!!
 requests.interceptors.request.use(config => {
     //config:配置对象,包含request中的所有配置信息，如：url、method、data、headers
+    nprogress.start();
     return config;
 });
 
 
 //响应拦截器：请求数据返回会执行
 requests.interceptors.response.use((res) => {
+    nprogress.done();
     return res.data;
 }, (error) => {
     return Promise.reject(new Error('failed'));
