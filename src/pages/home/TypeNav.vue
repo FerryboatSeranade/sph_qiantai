823d1299795c01eb9e94ade39465b2413ccc3dec
ferryboat <ferryboatseranade@gmail.com>
2023-03-18 18:25:54 +0800

    通过js实现鼠标hover 三级联动的第一级菜单时选定部分高亮 ，经查阅没有@hover这样的事件，但可通过vue自定义指令实现。 知识点：动态绑定类名。hover时class=cur,鼠标离开时则没有该类。

--- src/pages/home/TypeNav.vue
+++ src/pages/home/TypeNav.vue
@@ -1,7 +1,35 @@
 <template>
   <div class="type-nav">
     <div class="container">
-      <h2 class="all">全部商品分类</h2>
+      <div @mouseleave="resetIndex">
+        <h2 class="all">全部商品分类</h2>
+        <div class="sort">
+          <div class="all-sort-list2" >
+            <div class="item" v-for="(c1,index) in categoryList" :key="c1.categoryId" :class="{cur:currentIndex===index}">
+              <h3 @mouseover="changeIndex(index)" >
+                <a href="">{{ c1.categoryName }}</a>
+              </h3>
+              <div class="item-list clearfix">
+                <div class="subitem" v-for="(c2,index) in c1.categoryChild" :key="c2.categoryId">
+                  <dl class="fore">
+                    <dt>
+                      <a href="">{{ c2.categoryName }}</a>
+                    </dt>
+                    <dd>
+                      <em v-for="(c3,index) in c2.categoryChild" :key="c3.categoryId">
+                        <a href="">{{ c3.categoryName }}</a>
+                      </em>
+                    </dd>
+
+                  </dl>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
       <nav class="nav">
         <a href="###">服装城</a>
         <a href="###">美妆馆</a>
@@ -12,31 +40,7 @@
         <a href="###">有趣</a>
         <a href="###">秒杀</a>
       </nav>
-      <div class="sort">
-        <div class="all-sort-list2">
-          <div class="item" v-for="(c1,index) in categoryList" :key="c1.categoryId">
-            <h3>
-              <a href="">{{c1.categoryName}}</a>
-            </h3>
-            <div class="item-list clearfix">
-              <div class="subitem" v-for="(c2,index) in c1.categoryChild" :key="c2.categoryId">
-                <dl class="fore">
-                  <dt>
-                    <a href="">{{c2.categoryName}}</a>
-                  </dt>
-                  <dd>
-                    <em v-for="(c3,index) in c2.categoryChild" :key="c3.categoryId">
-                      <a href="">{{c3.categoryName}}</a>
-                    </em>
-                  </dd>
 
-                </dl>
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -53,12 +57,23 @@ export default {
     // name() {
     //
     // }
+    changeIndex(index) {
+      this.currentIndex = index
+    },
+    resetIndex() {
+      this.currentIndex = -1
+    }
   },
   mounted() {
     //dispatch
     // this.categoryList=this.getCategoryList()
     // console.log(this.categoryList,"####")
     this.getCategoryList()
+  },
+  data() {
+    return {
+      currentIndex: -1
+    }
   }
 }
 </script>
@@ -177,6 +192,10 @@ export default {
               display: block;
             }
           }
+        }
+
+        .cur {
+          background-color: skyblue;
         }
       }
     }
