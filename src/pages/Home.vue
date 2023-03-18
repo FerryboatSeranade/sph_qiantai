02d412a37bcc2e6f571847ef89191d58e4c2ae43
ferryboat <ferryboatseranade@gmail.com>
2023-03-18 13:13:18 +0800

    移除vuex demo

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="home">
     <h1>这是Home</h1>
-    <h2>当前购物车中的商品数量为：{{cartCount}}</h2>
-<!--    添加商品数量的设置，下拉列表-->
-    <select name="" id="" v-model.number="cardCountChange">
-      <option value=1>1</option>
-      <option value=2>2</option>
-      <option value=3>3</option>
-    </select>
-    <button @click="addCartCount(cardCountChange)">添加商品数量</button>
-    <button @click="subCartCount(cardCountChange)">减少商品数量</button>
 <!--    三级商品联动-->
     <TypeNav></TypeNav>
 <!--   轮播图 -->
