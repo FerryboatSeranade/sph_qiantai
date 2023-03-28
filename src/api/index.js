f4a4add6da4edf52fc4e3f3c7eaa5e3efa04133b
ferryboat <ferryboatseranade@gmail.com>
2023-03-28 11:35:06 +0800

    测试搜索接口是否能正常访问到数据

--- src/api/index.js
+++ src/api/index.js
@@ -5,3 +5,9 @@ import mockRequests from "@/api/mockRequests";
 export const reqCategoryList = () => requests.get("/product/getBaseCategoryList"); //获取商品分类列表
 export const reqBanners = () => mockRequests.get("/banners");
 export const reqFloors = () => mockRequests.get("/floors");
+
+export const reqGetSearchInfo = (params) => requests({
+    url: "/list",
+    method: "post",
+    data: params
+});
