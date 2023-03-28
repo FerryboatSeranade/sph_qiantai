2afd8381b4fe54937e3f453633e84757ba777947
ferryboat <ferryboatseranade@gmail.com>
2023-03-28 12:09:27 +0800

    adapt interface step2: vue三连环->mounted dispatch->浏览器插件查看vuex state

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ import Vuex from 'vuex'
 import typeNav from "@/store/typeNav";
 import listContainer from "@/store/listContainer";
 import floor from "@/store/floor";
+import search from "@/store/search";
 
 Vue.use(Vuex)
 //创建store对象
@@ -12,7 +13,8 @@ const store = new Vuex.Store({
     modules: {
         typeNav: typeNav,
         listContainer: listContainer,
-        floor: floor
+        floor: floor,
+        search: search
     }
 })
 export default store
---
+++ src/store/search.js
@@ -0,0 +1,31 @@
+import {reqGetSearchInfo} from "@/api";
+
+const state = {
+    searchList: {}
+}
+
+const mutations = {
+    //接收搜索信息
+    GET_SEARCH_LIST(state, searchList) {
+        state.searchList = searchList;
+    }
+}
+
+const actions = {
+    //获取搜索信息
+    async getSearchList({commit}, params) {
+        const result = await reqGetSearchInfo(params);
+        if (result.code === 200) {
+            commit('GET_SEARCH_LIST', result.data);
+        }
+    }
+}
+
+const getters = {}
+export default {
+    namespaced: true,
+    state,
+    mutations,
+    actions,
+    getters
+}
